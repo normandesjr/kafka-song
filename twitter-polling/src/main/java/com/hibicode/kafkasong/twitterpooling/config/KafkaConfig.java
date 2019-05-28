15fb0d096398488a6eb83f8dea54ef0aa15fa1bd
vinicius.sousa <vinicius.sousa@zup.com.br>
2019-05-28 13:12:53 -0300

    winner json deserializer only lower case

--- twitter-polling/src/main/java/com/hibicode/kafkasong/twitterpooling/config/KafkaConfig.java
+++ twitter-polling/src/main/java/com/hibicode/kafkasong/twitterpooling/config/KafkaConfig.java
@@ -1,5 +1,7 @@
 package com.hibicode.kafkasong.twitterpooling.config;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hibicode.kafkasong.twitterpooling.winner.Winner;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -33,23 +36,45 @@ public class KafkaConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, Winner> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "song");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConsumerFactory<String, Winner> consumerWinnerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "winner");
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
-                new JsonDeserializer<>(Winner.class));
+                new JsonDeserializer<>(Winner.class,objectMapper));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Winner> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Winner> kafkaListenerWinnerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Winner> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerWinnerFactory());
         return factory;
     }
 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setMessageConverter(new StringJsonMessageConverter());
+        return factory;
+    }
 }
